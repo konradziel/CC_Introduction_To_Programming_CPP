76b8e6b834e81fa726a1f18465071f23e518dd4c
konrad1503 <fresher002@gmail.com>
2022-11-18 10:56:01 +0100

    18.11

--- 18.11.2022/18.11/Zad2.cpp
+++ 18.11.2022/18.11/Zad2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
 	int parzyste[5] = { 2, 4, 6, 8, 10 };
-	cout << "Length of an array: " << sizeof(parzyste)/4;
+	int x = sizeof(parzyste)/sizeof(int);
+	cout << "Length of an array: " << x;
 
 }
